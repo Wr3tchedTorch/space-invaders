e8ec10c49ea421934b0b50d290c582674811f1fd
Wr3tchedTorch <ericmoura511@gmail.com>
2025-09-01 16:23:39 -0300

    feat: adjust bullet speeds and weapon fire rates; enhance upgrade pickup functionality with animations

--- scenes/agents/upgrades/UpgradePickUp.cs
+++ scenes/agents/upgrades/UpgradePickUp.cs
@@ -3,20 +3,67 @@ using SpaceInvaders.Assets.Scripts.Exceptions;
 using SpaceInvaders.Assets.Scripts.Interfaces;
 using SpaceInvaders.Scenes.Autoloads;
 using System;
+using System.Runtime.CompilerServices;
 
 namespace SpaceInvaders.Scenes.Agents.Upgrades;
 
 public partial class UpgradePickUp : CharacterBody2D
 {
+    private readonly StringName DisappearAnimationName = "disappear";
+
     [ExportGroup("Dependencies")]
     [Export] public Resource UpgradeResource { get; set; } = null!;
     [Export] public Area2D Area2D { get; set; } = null!;
+    [Export] public AnimationPlayer AnimationPlayer { get; set; } = null!;
+
+    [ExportGroup("Configuration")]
+    [Export] public float DelayBeforeDisappearing { get; set; } = 5;
 
     private bool pickedUp = false;
+
+    private double timePassedSinceSpawn;
+
+    private double AnimationStartDelay => DelayBeforeDisappearing * 0.3f;
 
     public override void _Ready()
     {
         Area2D.BodyEntered += OnBodyEntered;
+
+        var disappearTimer = new Timer()
+        {
+            OneShot = true,
+            Autostart = false,
+            WaitTime = DelayBeforeDisappearing
+        };
+        AddChild(disappearTimer);
+        disappearTimer.Timeout += () =>
+        {
+            if (!pickedUp)
+            {
+                QueueFree();
+            }
+        };
+        disappearTimer.Start();
+    }
+
+    public override void _Process(double delta)
+    {
+        timePassedSinceSpawn += delta;
+
+        if (timePassedSinceSpawn >= AnimationStartDelay)
+        {
+            PlayDisappearAnimation(timePassedSinceSpawn);
+        }        
+    }
+
+    private void PlayDisappearAnimation(double timePassedSinceSpawn)
+    {
+        if (!AnimationPlayer.IsPlaying())
+        {
+            AnimationPlayer.Play(DisappearAnimationName);
+        }
+        AnimationPlayer.SpeedScale = (float)(timePassedSinceSpawn / AnimationStartDelay);
+        GD.Print($"Animation Speed: {AnimationPlayer.SpeedScale}");
     }
 
     public override void _PhysicsProcess(double delta)
@@ -49,11 +96,11 @@ public partial class UpgradePickUp : CharacterBody2D
             throw new InvalidUpgradeTypeException(UpgradeResource.ResourceName);
         }
         QueueFree();
-    }    
+    }
 }
 
 public enum UpgradeType
 {
     Bullet,
-    Weapon    
+    Weapon
 }
